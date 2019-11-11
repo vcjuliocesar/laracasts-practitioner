db0f5a659f54da050fbd1b05e63a5564e87b9b75
julio cesar <vcjuliocesar@outlook.com>
2019-11-10 22:51:12 -0600

    php and html

---
+++ php-learning/php_and_html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?= "php-learning"; ?></title>
+  </head>
+  <style media="screen">
+    header{
+      background: #e3e3e3;
+      padding: 3em;
+      text-align: center;
+    }
+  </style>
+  <body>
+    <header>
+      Hello <?= htmlspecialchars($_GET['name']); ?>
+    </header>
+  </body>
+</html>
